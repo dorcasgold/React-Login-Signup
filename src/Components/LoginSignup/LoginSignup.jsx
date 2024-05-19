9bb5d4ce8638ad4a8ad804e6da8cfb45d323e410
odetayo dorcas <odetayodorcas123@gmail.com>
2024-05-19 17:01:46 +0100

    create jsx

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,44 +1,22 @@
+import React from 'react'
 import './LoginSignup.css'
 
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
-import { useState } from 'react'
+// import user_icon from '../Assets/person.png'
+// import email_icon from '../Assets/email.png'
+// import password_icon from '../Assets/password.png'
+// import { useState } from 'react'
+
 
 function LoginSignup() {
-  const [action, setAction] = useState("Sign Up")
   return (
-    <div className='container'>
-      <div className="header">
-        <div className="text">{action}</div>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs">
-        {
-          action === "Login" ? <div></div> : <div className="input">
-            <img src={user_icon} alt="user icon" />
-            <input type="text" placeholder='Name' />
-          </div>
-        }
-
-        <div className="input">
-          <img src={email_icon} alt="email icon" />
-          <input type="email" placeholder='Email Id' />
-        </div>
-        <div className="input">
-          <img src={password_icon} alt="password icon" />
-          <input type="password" placeholder='Password' />
-        </div>
-      </div>
-      {
-        action !== "Login" ? <div></div> : <div className="forgot-password">Lost Password? <span onClick={() => setAction('Reset Password')}>Click Here</span></div>
-
-      }
-      <div className="submit-container">
-        <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => setAction("Sign Up")}>Sign Up</div>
-
-        <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => setAction("Login")}>Login</div>
-      </div>
+    <div className="container">
+      <header>
+        <h1>Sign Up</h1>
+      </header>
+      <div className="inputs"></div>
+      <div className="checkbox"></div>
+      <div className="btn"></div>
+      <div className="bottomLink"></div>
     </div>
   )
 }
