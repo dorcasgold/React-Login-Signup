d58ade691feae670ee1139eb38d1377db068cd0b
odetayo dorcas <odetayodorcas123@gmail.com>
2024-05-20 07:14:39 +0100

    assign validationErrors for inputs

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -5,27 +5,65 @@ import { useState } from 'react'
 
 function LoginSignup() {
   const [title, setTitle] = useState('Sign Up')
+  // store input value
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    password: '',
+    email: '',
+    confirmPassword: '',
+  })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData, [name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const validationErrors = {}
+    if (!formData.firstName.trim()) {
+      validationErrors.firstName = 'FirstName is required'
+    }
+    if (!formData.lastName.trim()) {
+      validationErrors.lastName = 'LastName is required'
+    }
+    if (!formData.email.trim()) {
+      validationErrors.email = 'email is required'
+    } else if (!/\S+@\S\.\S+/.test(formData.email)) {
+      validationErrors.email = "email is not valid"
+    }
+    if (!formData.password.trim()) {
+      validationErrors.password = 'password is required'
+    } else if (formData.password.length < 7) {
+      validationErrors.email = "password should be at least 7 char"
+    }
+    if (formData.confirmPassword !== formData.password) {
+      validationErrors.confirmPassword = 'Password not match'
+    }
+  }
   return (
-    <div className="container flex flex-col items-center">
+    <form onSubmit={handleSubmit} className="container flex flex-col items-center">
       <header className='my-4 '>
         <h1 className='text-3xl font-bold'>{title}</h1>
       </header>
       <div className="inputs">
         {title === 'Sign In' ? <div></div> :
           <div className="input flex gap-5">
-            <input type="text" placeholder='FirstName' />
-            <input type="text" placeholder='LastName' />
+            <input type="text" name='firstName' autoComplete='off' placeholder='FirstName' onChange={handleChange} />
+            <input type="text" name='lastName' autoComplete='off' placeholder='LastName' />
           </div>
         }
 
         <div className="input">
-          <input type="email" placeholder='email' />
+          <input type="email" name='email' autoComplete='off' placeholder='email' />
         </div>
         <div className="input">
-          <input type="password" placeholder='password' />
+          <input type="password" name='password' placeholder='password' />
         </div>
         {title === 'Sign In' ? <div></div> : <div className="input flex">
-          <input type="password" placeholder='Confirm Password' />
+          <input type="password" name='confirmPassword' placeholder='Confirm Password' />
         </div>}
 
       </div>
@@ -64,7 +102,7 @@ function LoginSignup() {
       }
 
 
-    </div >
+    </form>
   )
 }
 
